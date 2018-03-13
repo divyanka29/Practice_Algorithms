b18e120fbf8acb221c5fd6b1bb1c59ebca84aba4
Divyanka Bose <divyankabose@Divyankas-MacBook-Pro.local>
2018-03-13 07:05:36 -0600

    Add path tree

---
+++ Practice_Algorithms/PathWithSumInTree.cpp
@@ -0,0 +1,7 @@
+/*
+You are given a binary tree in which each node contains an integer value(which might be positive or negative). 
+Design an algorithm to count the number of paths that sum to a given value. 
+The path does not need to start at the root or a leaf, but it must go downwards (travelling only from parent to child nodes)
+*/
+
+/**/
